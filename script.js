380c2c977d2b9cbe299280cf879f6c8025566c80
Kobe Bryant <manuthegreat03@gmail.com>
2017-08-31 21:25:11 -0400

    First Release

---
+++ script.js
@@ -0,0 +1,96 @@
+function clickCounter() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)-1;
+        } else {
+            localStorage.clickcount = 7;
+        }
+        document.getElementById("result").innerHTML = localStorage.clickcount;
+    } else { 
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounter1() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount1) {
+            localStorage.clickcount1 = Number(localStorage.clickcount1)-1;
+        } else {
+            localStorage.clickcount1 = 7;
+        }
+        document.getElementById("result1").innerHTML = localStorage.clickcount1;
+    } else { 
+        document.getElementById("result1").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounter2() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount2) {
+            localStorage.clickcount2 = Number(localStorage.clickcount2)-1;
+        } else {
+            localStorage.clickcount2 = 7;
+        }
+        document.getElementById("result2").innerHTML = localStorage.clickcount2;
+    } else { 
+        document.getElementById("result2").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounter3() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount3) {
+            localStorage.clickcount3 = Number(localStorage.clickcount3)-1;
+        } else {
+            localStorage.clickcount3 = 7;
+        }
+        document.getElementById("result3").innerHTML = localStorage.clickcount3;
+    } else { 
+        document.getElementById("result3").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounter4() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount4) {
+            localStorage.clickcount4 = Number(localStorage.clickcount4)-1;
+        } else {
+            localStorage.clickcount4 = 7;
+        }
+        document.getElementById("result4").innerHTML = localStorage.clickcount4;
+    } else { 
+        document.getElementById("result4").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounter5() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount5) {
+            localStorage.clickcount5 = Number(localStorage.clickcount5)-1;
+        } else {
+            localStorage.clickcount5 = 7;
+        }
+        document.getElementById("result5").innerHTML = localStorage.clickcount5;
+    } else { 
+        document.getElementById("result5").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounter6() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount6) {
+            localStorage.clickcount6 = Number(localStorage.clickcount6)-1;
+        } else {
+            localStorage.clickcount6 = 7;
+        }
+        document.getElementById("result6").innerHTML = localStorage.clickcount6;
+    } else { 
+        document.getElementById("result6").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+function clickCounter7() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount7) {
+            localStorage.clickcount7 = Number(localStorage.clickcount7)-1;
+        } else {
+            localStorage.clickcount7 = 7;
+        }
+        document.getElementById("result7").innerHTML = localStorage.clickcount7;
+    } else { 
+        document.getElementById("result7").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
